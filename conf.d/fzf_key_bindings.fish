11f71dfbe94b593b867ace4c2bfa4e12869b1ec6
Andrew Jarrett <ahrjarrett@gmail.com>
2019-04-15 17:18:29 -0500

    update bindings

--- conf.d/fzf_key_bindings.fish
+++ conf.d/fzf_key_bindings.fish
@@ -1,9 +1,11 @@
 if test "$FZF_LEGACY_KEYBINDINGS" -eq 1
-    bind \ct '__fzf_find_file'
+    #bind \ct '__fzf_find_file'
+    bind \cs '__fzf_find_file'
     bind \cr '__fzf_reverse_isearch'
     bind \ec '__fzf_cd'
     bind \eC '__fzf_cd --hidden'
-    bind \cg '__fzf_open'
+    #bind \cg '__fzf_open'
+    bind \eo '__fzf_open'
     bind \co '__fzf_open --editor'
 
     if bind -M insert >/dev/null 2>/dev/null
